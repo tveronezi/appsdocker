2d25fc1e9633b174ad2eaf80586fa9f044787374
Thiago Veronezi <tveronezi@apache.org>
2015-02-01 17:09:23 -0500

    adding springchat

--- apps/proxy/run.sh
+++ apps/proxy/run.sh
@@ -2,7 +2,7 @@
 docker kill proxy || true
 docker rm proxy || true
 docker build -t proxy .
-docker run -d -p 80:81 -p 8000:8000 \
+docker run -d -p 80:80 -p 8000:8000 \
   --link=tomeetest:tomeetest \
   --link=jchat:jchat \
   --link=springchat:springchat \
--- apps/run.sh
+++ apps/run.sh
@@ -2,9 +2,11 @@
 
 cd buildbox && ./run.sh && cd ..
 cd apps-base && ./run.sh && cd ..
+
 cd tomeetest && ./run.sh && cd ..
 cd activemq && ./run.sh && cd ..
 cd jchatdb && ./run.sh && cd ..
 cd jchat && ./run.sh && cd ..
+cd springchatdb && ./run.sh && cd ..
+cd springchat && ./run.sh && cd ..
 cd proxy && ./run.sh && cd ..
-
